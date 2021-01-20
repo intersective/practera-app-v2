36cf3842fe907799830e278529ee18f8318094d2
ronak <ronak@practera.com>
2021-01-21 09:29:53 +1100

    updateFCMMessageingService

--- android/app/src/main/java/com/practera/appv2/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/practera/appv2/MyFirebaseMessagingService.java
@@ -6,21 +6,43 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 //the class extending FirebaseInstanceIdService
-public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+public class MyFirebaseMessagingService extends FirebaseInstanceIdService {
  
- 
-    //this method will be called
-    //when the token is generated
-    @Override
-    public void onTokenRefresh() {
-        super.onTokenRefresh();
- 
-        //now we will have the token
-        String token = FirebaseInstanceId.getInstance().getToken();
- 
-        //for now we are displaying the token in the log
-        //copy it as this method is called only when the new token is generated
-        //and usually new token is only generated when the app is reinstalled or the data is cleared
-        Log.d("MyRefreshedToken", token);
-    }
+ FirebaseMessaging.getInstance().getToken()
+    .addOnCompleteListener(new OnCompleteListener<String>() {
+        @Override
+        public void onComplete(@NonNull Task<String> task) {
+          if (!task.isSuccessful()) {
+            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+            return;
+          }
+
+          // Get new FCM registration token
+          String token = task.getResult();
+
+          // Log and toast
+          String msg = getString(R.string.msg_token_fmt, token);
+          Log.d(TAG, msg);
+          Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+    });
+
+    /**
+ * There are two scenarios when onNewToken is called:
+ * 1) When a new token is generated on initial app startup
+ * 2) Whenever an existing token is changed
+ * Under #2, there are three scenarios when the existing token is changed:
+ * A) App is restored to a new device
+ * B) User uninstalls/reinstalls the app
+ * C) User clears app data
+ */
+@Override
+public void onNewToken(String token) {
+    Log.d(TAG, "Refreshed token: " + token);
+
+    // If you want to send messages to this application instance or
+    // manage this apps subscriptions on the server side, send the
+    // FCM registration token to your app server.
+    sendRegistrationToServer(token);
+}
 }
